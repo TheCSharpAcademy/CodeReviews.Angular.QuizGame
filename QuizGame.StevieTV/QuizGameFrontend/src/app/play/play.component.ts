6a6dff9a0428977c03e78814097b2707d384f3b3
Stevie Robinson <stevie.robinson@gmail.com>
2024-03-23 20:40:03 +0100

    added pagination to quizzes, questions and play

--- QuizGame.StevieTV/QuizGameFrontend/src/app/play/play.component.ts
+++ QuizGame.StevieTV/QuizGameFrontend/src/app/play/play.component.ts
@@ -1,8 +1,10 @@
-import { NgForOf } from "@angular/common";
-import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import {MatCardModule} from '@angular/material/card';
 import {MatListModule} from '@angular/material/list';
 import { QuizService } from "../quiz.service";
+import { MatPaginator, MatPaginatorModule, PageEvent } from '@angular/material/paginator';
+
 
 @Component({
   selector: 'app-play',
@@ -10,19 +12,37 @@ import { QuizService } from "../quiz.service";
   imports: [
     MatCardModule,
     MatListModule,
-    NgForOf
+    CommonModule,
+    MatPaginatorModule
   ],
   templateUrl: './play.component.html',
   styleUrl: './play.component.css'
 })
 export class PlayComponent implements OnInit {
 
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
   quizzes: any;
+  pagedQuizzes: any;
+  dataLength: number = 0;
 
   constructor(public quizService: QuizService ) {
   }
 
   ngOnInit() {
-    this.quizService.getQuizzes().subscribe(res => this.quizzes = res);
+    this.quizService.getQuizzes().subscribe(res => {
+      this.quizzes = res
+      this.pagedQuizzes = this.quizzes.slice(0,5);
+      this.pagedQuizzes.paginator = this.paginator;
+      this.dataLength = this.quizzes.length;
+    });
+  }
+
+  OnPageChange(event: PageEvent) {
+    let from = event.pageIndex * event.pageSize;
+    let to = from + event.pageSize;
+    if (to > this.dataLength) {
+      to = this.dataLength;
+    }
+    this.pagedQuizzes = this.quizzes.slice(from, to);
   }
 }
--- QuizGame.StevieTV/QuizGameFrontend/src/app/questions/questions.component.ts
+++ QuizGame.StevieTV/QuizGameFrontend/src/app/questions/questions.component.ts
@@ -1,24 +1,44 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatCardModule } from '@angular/material/card';
 import { MatListModule } from '@angular/material/list';
 import { ActivatedRoute } from "@angular/router";
 import { QuizService } from '../quiz.service';
+import { MatPaginator, MatPaginatorModule, PageEvent } from '@angular/material/paginator';
+
 
 @Component({
   selector: 'app-questions',
   standalone: true,
-  imports: [CommonModule, MatCardModule, MatListModule],
+  imports: [CommonModule, MatCardModule, MatListModule, MatPaginatorModule],
   templateUrl: './questions.component.html',
   styleUrls: ['./questions.component.css'],
 })
 export class QuestionsComponent implements OnInit {
 
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
   questions: any;
-  constructor(public quizService: QuizService, private route: ActivatedRoute) {}
+  pagedQuestions: any;
+  dataLength: number = 0;
+  constructor(public quizService: QuizService, private route: ActivatedRoute) {
+  }
 
   ngOnInit() {
     let quizId = Number(this.route.snapshot.paramMap.get('quizId'));
-    this.quizService.getQuestions(quizId).subscribe(res => this.questions = res);
+    this.quizService.getQuestions(quizId).subscribe(res => {
+      this.questions = res;
+      this.pagedQuestions = this.questions.slice(0,5);
+      this.pagedQuestions.paginator = this.paginator;
+      this.dataLength = this.questions.length;
+    });
+  }
+
+  OnPageChange(event: PageEvent) {
+    let from = event.pageIndex * event.pageSize;
+    let to = from + event.pageSize;
+    if (to > this.dataLength) {
+      to = this.dataLength;
+    }
+    this.pagedQuestions = this.questions.slice(from, to);
   }
 }
