6a93b1f6449f6f14bb44966ac0bdd13927d426e1
Michiel Vergauwen <vergauwenmichiel@gmail.com>
2024-05-28 16:52:21 +0200

    Added models

---
+++ QuizGame.MicVerg/QuizGameAPI/Models/Question.cs
@@ -0,0 +1,14 @@
+﻿namespace QuizGameAPI.Models
+{
+    public class Question
+    {
+        public int Id { get; set; }
+        public string QuizQuestion { get; set; } = "";
+        public string Answer1 { get; set; } = "";
+        public string Answer2 { get; set; } = "";
+        public string Answer3 { get; set; } = "";
+        public string CorrectAnswer { get; set; } = "";
+        public int QuizId { get; set; }
+        public Quiz Quiz { get; set; } = null!;
+    }
+}
