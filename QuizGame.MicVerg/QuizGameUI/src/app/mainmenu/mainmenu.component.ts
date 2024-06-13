08bd86954622059a2a3732f8d5f81ab2f9dd4750
Michiel Vergauwen <vergauwenmichiel@gmail.com>
2024-06-13 16:50:37 +0200

    styling, starting dialogs next

--- QuizGame.MicVerg/QuizGameUI/src/app/mainmenu/mainmenu.component.ts
+++ QuizGame.MicVerg/QuizGameUI/src/app/mainmenu/mainmenu.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import {MatButtonModule} from '@angular/material/button';
 import {MatCardModule} from '@angular/material/card';
+import {MatIconModule} from '@angular/material/icon';
 
 @Component({
   selector: 'app-mainmenu',
   standalone: true,
-  imports: [MatButtonModule, MatCardModule],
+  imports: [MatButtonModule, MatCardModule, MatIconModule],
   templateUrl: './mainmenu.component.html',
   styleUrl: './mainmenu.component.css'
 })
