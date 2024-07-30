1ff8dc92e1922c219e8a5f646349c7b25e3d8f3b
Michiel Vergauwen <vergauwenmichiel@gmail.com>
2024-07-30 12:13:11 +0200

    fixed scorehistory paginator

--- QuizGame.MicVerg/QuizGameUI/src/app/scorehistory/scorehistory.component.ts
+++ QuizGame.MicVerg/QuizGameUI/src/app/scorehistory/scorehistory.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, AfterViewInit } from '@angular/core';
 import {MatTableModule} from '@angular/material/table';
 import { Game } from '../game.model';
 import { QuizServiceService } from '../quiz-service.service';
@@ -7,21 +7,37 @@ import { MainmenuComponent } from '../mainmenu/mainmenu.component';
 import { firstValueFrom } from 'rxjs';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { Inject } from '@angular/core';
+import { MatPaginatorModule, PageEvent } from '@angular/material/paginator';
+import { CommonModule } from '@angular/common';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatPaginator } from '@angular/material/paginator';
 
 @Component({
   selector: 'app-scorehistory',
   standalone: true,
-  imports: [MatTableModule],
+  imports: [MatTableModule, MatDialogModule, MatPaginatorModule, CommonModule, MatPaginator],
   templateUrl: './scorehistory.component.html',
   styleUrl: './scorehistory.component.css'
 })
 export class ScorehistoryComponent {
-
+  dataSource = new MatTableDataSource<Game>();
   displayedColumns: string[] = ['id', 'playerName', 'score', 'quizId']
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
 
   constructor(private quizService: QuizServiceService, public dialog: MatDialog, public dialogRef: MatDialogRef<ScorehistoryComponent>,
     @Inject(MAT_DIALOG_DATA) public games: Game[]){
   }
 
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+  }
+
+  ngOnInit() {
+    this.quizService.getGames().subscribe((data: Game[]) => {
+      this.dataSource.data = data;
+    });
   
+}
 }
