14e4308a559ffef2eba0519f0b24e412a6a46f5b
Michiel Vergauwen <vergauwenmichiel@gmail.com>
2024-08-12 11:37:18 +0200

    added form to dialog

--- QuizGame.MicVerg/QuizGameUI/src/app/addquizdialog/addquizdialog.component.ts
+++ QuizGame.MicVerg/QuizGameUI/src/app/addquizdialog/addquizdialog.component.ts
@@ -1,12 +1,25 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
+import { Quiz } from '../quiz.model';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+
 
 @Component({
   selector: 'app-addquizdialog',
   standalone: true,
-  imports: [],
+  imports: [MatDialogModule, MatPaginatorModule, MatPaginator, CommonModule, MatFormFieldModule, MatInputModule, MatButtonModule],
   templateUrl: './addquizdialog.component.html',
   styleUrl: './addquizdialog.component.css'
 })
 export class AddquizdialogComponent {
 
+  questionsCount = Array.from({length: 10}, (_, i) => i + 1);
+
+  submitQuiz(){
+
+  }
 }
